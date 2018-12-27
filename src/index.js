dece14ee821f031d4471a53469799f1d999c93a1
w4wycliff <w4wycliff@gmail.com>
2018-12-27 13:16:14 +0300

    chore(setup): setup redux
    -install redux
    -install react-redux
    -install redux-thunk
    
    [Finishes #162855932]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
  import React from 'react';
  import ReactDom from 'react-dom';
+ import { Provider } from 'react-redux';
  import App from './components/App';
+ import store from './store';
 
- ReactDom.render(<App />, document.getElementById('app'));
+ ReactDom.render(
+<Provider store={store}>
+ <App />
+</Provider>, 
+ document.getElementById('app'));
 
