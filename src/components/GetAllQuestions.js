65943b47347fd17a3329da9cfc3e66068b5b44ec
wycliffkas <42246312+wycliffkas@users.noreply.github.com>
2019-01-04 08:47:00 +0300

    Update GetAllQuestions.js

--- src/components/GetAllQuestions.js
+++ src/components/GetAllQuestions.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import PropTypes from "prop-types";
 import { fetchQuestions } from '../actions/questions';
 import "../App.css";
 
-class getAllQuestions extends Component {
+class GetAllQuestions extends Component {
 
     componentWillMount(){
         this.props.fetchQuestions();
     }
 
+    
     render() {
-        const questionsItems = (this.props.questions).map((question) => (
+        const { questions } = this.props;
+        const questionsItems = (questions).map((question) => (
             <div key={question.questionId} className="panel panel-default">
                     
                 <NavLink to={{pathname: `/questions/${question.questionId}`}}>
@@ -23,16 +26,44 @@ class getAllQuestions extends Component {
         ));
         return (
             <div>
-                <h3>All Questions</h3>
-                { questionsItems }
+                <div>
+                    {questions.length > 1 ? 
+                    (
+                        <div>
+                    <h3>All Questions</h3> 
+                    <div>{questionsItems}</div>
+                    </div>
+                    )
+                    : <div className="text-center">
+                    <img
+                      src="https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif"
+                      className="loading__image"
+                      alt=""
+                    />
+                  </div> }
+                </div>
+               
                 
             </div>
-        );
+        )
+        
+
     }
 }
 
-const mapStateToProps = state => ({
+GetAllQuestions.propTypes = {
+    questions: PropTypes.array.isRequired,
+    fetchQuestions: PropTypes.func.isRequired,
+
+};
+
+export const mapStateToProps = state => ({
     questions: state.questions.questions,
 });
 
-export default connect(mapStateToProps, { fetchQuestions })(getAllQuestions);
+export const mapDispatchToProps = dispatch => ({
+    fetchQuestions: () => dispatch(fetchQuestions())
+});
+
+export { GetAllQuestions };
+export default connect(mapStateToProps, mapDispatchToProps)(GetAllQuestions);
